fb843bb118eb879ca949c3ffcb399508b65f6c95
shakiz <activities335@gmail.com>
2024-01-13 13:39:18 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/ride/pathaoapp/pathao_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ride.pathaoapp.pathao_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/HomePage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
