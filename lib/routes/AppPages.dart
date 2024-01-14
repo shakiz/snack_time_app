ea7c8b04a037413d1eff77407e96e7ef9cb79bb0
shakiz <activities335@gmail.com>
2024-01-14 15:19:42 +0600

    Add get for navigation and appPages, routed were added

---
+++ lib/routes/AppPages.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:pathao_app/features/home/HomePage.dart';
+import 'package:pathao_app/features/onboarding/OnBoardingPage.dart';
+import 'package:pathao_app/routes/AppRoutes.dart';
+
+class AppPages {
+  static const INITIAL_ROUTE = AppRoutes.APP_ROUTE_ONBOARDING;
+
+  static final routes = [
+    GetPage(name: AppRoutes.APP_ROUTE_ONBOARDING, page: () => OnBoardingPage()),
+    GetPage(name: AppRoutes.APP_ROUTE_HOME, page: () => HomePage()),
+  ];
+}
---
+++ lib/routes/AppRoutes.dart
@@ -0,0 +1,9 @@
+abstract class AppRoutes {
+  static const APP_ROUTE_HOME = _Paths.HOME;
+  static const APP_ROUTE_ONBOARDING = _Paths.ONBOARDING;
+}
+
+abstract class _Paths {
+  static const HOME = "/home";
+  static const ONBOARDING = "/onboarding";
+}
