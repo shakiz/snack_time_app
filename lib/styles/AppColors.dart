f1e5cb596e1d9f587a27eceb1a50afc77e3c01aa
shakiz <activities335@gmail.com>
2024-01-14 13:08:19 +0600

    Add app theme colors

---
+++ lib/styles/AppColors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color colorPrimary = Color(0xFFFB8C00);
+  static const Color colorSuccess = Color(0xFF4CAF50);
+  static const Color colorError = Color(0xFFF44336);
+
+  static const Color colorOnPrimary = Color(0xFFFFFFFF);
+  static const Color colorOnPrimaryBg = Color(0xFF131313);
+
+  static const Color colorSecondary = Color(0xFFCACACA);
+
+  static const Color colorSurface_5 = Color(0xFFF8F8F8);
+  static const Color colorSurface_1 = Color(0xFFEBEBEB);
+  static const Color colorSurface_1_5 = Color(0xFFFEE8CC);
+  static const Color colorBorderDivider = Color(0xFFF2F2F2);
+}
