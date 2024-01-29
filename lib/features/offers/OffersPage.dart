120adc852b733dbeecd5be7ce29e59b300da4fc0
shakiz <activities335@gmail.com>
2024-01-29 13:57:23 +0600

    Update local data and UI for offers

--- lib/features/offers/OffersPage.dart
+++ lib/features/offers/OffersPage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
+import 'package:snack_time/common_widgets/OffersItem.dart';
+import 'package:snack_time/gen/assets.gen.dart';
+import '../../constants/ConstantValues.dart';
+import '../../styles/AppColors.dart';
 import 'OffersViewModel.dart';
 
 class OffersPage extends StatefulWidget {
@@ -16,7 +21,55 @@ class _OffersPageState extends State<OffersPage> {
         (BuildContext context, OffersViewModel offersViewModel, Widget? child) {
       return SafeArea(
           child: Scaffold(
-        body: Container(),
+        body: ListView(
+          padding: const EdgeInsets.all(ConstantValues.Padding_16),
+          children: [
+            const Text(
+              "Offers Gallery",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "Roboto",
+                  decoration: TextDecoration.none,
+                  color: AppColors.colorOnPrimaryBg,
+                  fontSize: ConstantValues.Font_Size_20),
+            ),
+            const SizedBox(
+              height: ConstantValues.Margin_16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Recommended For You",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontFamily: "Roboto",
+                      decoration: TextDecoration.none,
+                      color: AppColors.colorOnPrimaryBg,
+                      fontSize: ConstantValues.Font_Size_16),
+                ),
+                SvgPicture.asset(Assets.icons.icFilter)
+              ],
+            ),
+            const SizedBox(
+              height: ConstantValues.Margin_16,
+            ),
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemCount: offersViewModel.offers.length,
+              itemBuilder: (context, index) {
+                return OfferItem(offers: offersViewModel.offers[index]);
+              },
+            ),
+            const SizedBox(
+              height: ConstantValues.Margin_24,
+            )
+          ],
+        ),
       ));
     });
   }
--- lib/features/offers/OffersViewModel.dart
+++ lib/features/offers/OffersViewModel.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/cupertino.dart';
 
-class OffersViewModel extends ChangeNotifier {}
+import '../../data/model/Offers.dart';
+import '../../gen/assets.gen.dart';
+
+class OffersViewModel extends ChangeNotifier {
+  List<Offers> offers = [
+    Offers("10-40% OFF", "Flat Discounts on Every Purchase!",
+        Assets.images.icMcdonalds.path, "4.5", "Food", "15 Days"),
+    Offers("40-60% OFF", "For First Time Users",
+        Assets.images.icBurgerKing.path, "4.2", "Fast Food", "9 Days"),
+    Offers("40% Cashback", "For Repeat Orders", Assets.images.icKfc.path, "4.6",
+        "Culinary", "4 Days"),
+    Offers("Get 10Tk Cashback", "New Users Only",
+        Assets.images.icCheeseSlice.path, "4.8", "New Customer", "20 Days")
+  ];
+}
