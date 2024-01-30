b36d169cd94d003b780a7a0c267b8b23796ac2e8
shakiz <activities335@gmail.com>
2024-01-31 00:00:52 +0600

    Update main app

---
+++ lib/SnackTimeApp.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+import 'features/MainViewModel.dart';
+import 'features/details/SnackItemDetailsViewModel.dart';
+import 'features/home/HomeViewModel.dart';
+import 'features/offers/OffersViewModel.dart';
+import 'features/onboarding/OnBoardingViewModel.dart';
+import 'features/orders/OrdersViewModel.dart';
+import 'features/profile/ProfileViewModel.dart';
+import 'routes/AppPages.dart';
+import 'routes/AppRoutes.dart';
+
+class SnackTimeApp extends StatelessWidget {
+  const SnackTimeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => OnBoardingViewModel()),
+        ChangeNotifierProvider(create: (context) => MainViewModel()),
+        ChangeNotifierProvider(create: (context) => HomeVIewModel()),
+        ChangeNotifierProvider(create: (context) => OffersViewModel()),
+        ChangeNotifierProvider(create: (context) => OrdersViewModel()),
+        ChangeNotifierProvider(
+            create: (context) => SnackItemDetailsViewModel()),
+        ChangeNotifierProvider(create: (context) => ProfileViewModel())
+      ],
+      child: GetMaterialApp(
+        title: 'Snack Time',
+        initialRoute: AppRoutes.APP_ROUTE_ONBOARDING,
+        getPages: AppPages.routes,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:snack_time/features/MainViewModel.dart';
-import 'package:snack_time/features/details/SnackItemDetailsViewModel.dart';
-import 'package:snack_time/features/home/HomeViewModel.dart';
-import 'package:snack_time/features/offers/OffersViewModel.dart';
-import 'package:snack_time/features/onboarding/OnBoardingViewModel.dart';
-import 'package:snack_time/features/orders/OrdersViewModel.dart';
-import 'package:snack_time/features/profile/ProfileViewModel.dart';
-import 'package:snack_time/routes/AppPages.dart';
-import 'package:snack_time/routes/AppRoutes.dart';
-import 'package:provider/provider.dart';
+import 'package:snack_time/SnackTimeApp.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => OnBoardingViewModel()),
-        ChangeNotifierProvider(create: (context) => MainViewModel()),
-        ChangeNotifierProvider(create: (context) => HomeVIewModel()),
-        ChangeNotifierProvider(create: (context) => OffersViewModel()),
-        ChangeNotifierProvider(create: (context) => OrdersViewModel()),
-        ChangeNotifierProvider(
-            create: (context) => SnackItemDetailsViewModel()),
-        ChangeNotifierProvider(create: (context) => ProfileViewModel())
-      ],
-      child: GetMaterialApp(
-        title: 'Snack Time',
-        initialRoute: AppRoutes.APP_ROUTE_ONBOARDING,
-        getPages: AppPages.routes,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-      ),
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SnackTimeApp());
 }
